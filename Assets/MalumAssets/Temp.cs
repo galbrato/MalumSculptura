7fa77d783a669bbe79f87feb318c2545efafa310
Gabriel Simmel Nascimento <galbrato@gmail.com>
2018-09-22 18:53:57 -0300

    initial commit with project

---
+++ Assets/MalumAssets/Temp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.Gaming;
+
+public class Temp : MonoBehaviour {
+	GazeAware x;
+
+	void  Start() {
+		x = gameObject.GetComponent<GazeAware> ();
+	}
+
+	void Update () {
+		if(x.HasGazeFocus) {
+			Debug.Log (gameObject.tag);
+		}
+	}
+}
