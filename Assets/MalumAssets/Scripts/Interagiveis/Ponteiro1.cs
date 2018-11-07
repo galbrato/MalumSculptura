b69d455ad2318a88cc430d88254e32849438a952
Gabriel Simmel Nascimento <galbrato@gmail.com>
2018-11-07 00:27:20 -0200

    Implementei a estatua atormentada matar o palyer, e coloquei seu modelo

--- Assets/MalumAssets/Scripts/Interagiveis/Ponteiro1.cs
+++ Assets/MalumAssets/Scripts/Interagiveis/Ponteiro1.cs
@@ -5,18 +5,16 @@ using UnityEngine;
 public class Ponteiro1 : MonoBehaviour {
 	[HideInInspector]
 	public Relogio relogio;
-	private Transform trans;
 	private float umCiclo;
 
 	//funcao de start chamada depois de achar relogio
 	public void comeco(int divisaoDoCiclo, Relogio rel){
 		relogio = rel;
-		trans = GetComponent<Transform>();
 		umCiclo = relogio.tempoDeJogo / divisaoDoCiclo;
 	}
 	
 	void Update(){
-		trans.localEulerAngles = new Vector3(trans.localEulerAngles.x,((relogio.tempo) % umCiclo / umCiclo) * 360, trans.localEulerAngles.z);
+		transform.localEulerAngles = new Vector3(transform.localEulerAngles.x,((relogio.tempo) % umCiclo / umCiclo) * 360, transform.localEulerAngles.z);
 
 	}
 }
--- Assets/MalumAssets/Scripts/Lanterna.cs
+++ Assets/MalumAssets/Scripts/Lanterna.cs
@@ -12,25 +12,21 @@ public class Lanterna : MonoBehaviour {
     public int nCircles = 3;
     public int addicionalAngle = 5;
     private EnemyBehaviour saveEnemy;
+    private cryingStatue saveChorona;
     public bool debug = false;
 
     public bool isTurnedOn = true;
 
     //variaveis da bateria
-    public float MaxBaterry = 100f;
-    public float BatteryDrainPerSecond = 1f;
-    public float BatteryRechargePerSecond = 3f;
-    public float ActualBattery = 100f;
-
-    public float HoldDelay = 0.1f;
-    private float HoldDelayCaunter;
+    public float BaterryDuration = 60f;
+    public float BaterryRecoveryDuration = 30f;
+    public float BaterryCounter;
 
     public float LightDecressPoint = 5;
 
     public static Lanterna instance = null;
 
     private float OriginalRange;
-
     private void Awake() {
         if (instance == null)
             instance = this;
@@ -39,7 +35,7 @@ public class Lanterna : MonoBehaviour {
     }
 
     void Start() {
-        HoldDelayCaunter = HoldDelay;
+        BaterryCounter = BaterryDuration;
         myLight = gameObject.GetComponent<Light>();
         OriginalRange = myLight.range;
     	//jogador = FindObjectOfType<Player>().GetComponent<Player>();
@@ -59,21 +55,26 @@ public class Lanterna : MonoBehaviour {
             Recharge();
         }
 
-        if(isTurnedOn && ActualBattery > 0f) {
-            ActualBattery -= BatteryDrainPerSecond * Time.deltaTime;
+        if(isTurnedOn && BaterryCounter > 0f) {
+            BaterryCounter -= Time.deltaTime;
         }
 
-        if(ActualBattery < 0) {
+        if(BaterryCounter < 0) {
             LightOff();
         }
 
-        if((ActualBattery/MaxBaterry)*100 < LightDecressPoint) {
-            myLight.range = OriginalRange * (ActualBattery / (MaxBaterry * (LightDecressPoint/100)));
+        if(BaterryCounter < LightDecressPoint) {
+            myLight.range = OriginalRange * (BaterryCounter / LightDecressPoint);
         } else {
             myLight.range = OriginalRange;
         }
 
         if (isTurnedOn == false) return;
+        Look();
+        Detect();
+    }
+
+    void Look() {
         Vector3 olhando;
         Ray ray;
 
@@ -87,39 +88,56 @@ public class Lanterna : MonoBehaviour {
 				Debug.Log ("Seus olhos não podem ser detectados");
 			}*/
         }
-
         if (seekMouse) gameObject.transform.rotation = Quaternion.LookRotation(ray.direction);
         else gameObject.transform.rotation = Camera.main.transform.rotation;
+    }
 
-        //jogador.dirVisao = ray.direction;
-        //Debug.Log(ray.direction);
-        Vector3 dir = (seekMouse ? ray.direction : Camera.main.transform.forward);
-        RayDetection(dir, myLight.range);
-        for (int c = 1; c < nCircles; c++) {
 
-            Vector3 dir2 = Quaternion.AngleAxis(((myLight.spotAngle + addicionalAngle) / (2 * nCircles)) * c, transform.up) * dir;
-            int nRaysAux = 1 + ((nRays - 1) / (nCircles - 1)) * c;
-            for (int i = 0; i < nRaysAux; i++) {
-                //rotacionar
-                Vector3 dir3 = Quaternion.AngleAxis(i * (360f / nRaysAux), transform.forward) * dir2;
-                RayDetection(dir3, myLight.range);
-                //mandar o inimigo ficar paradão
+    void Detect() { 
+        Vector3 dir = Camera.main.transform.forward;
+        List<Collider> cols = ConeShowerDetection(transform, dir, myLight.range, myLight.spotAngle + addicionalAngle, nCircles, nRays);
+        foreach (Collider col in cols) {
+            if (col.CompareTag("Enemy")) {
+                if (saveEnemy == null || saveEnemy.name != col.name) {
+                    saveEnemy = col.gameObject.GetComponent<EnemyBehaviour>();
+                }
+                saveEnemy.Stop();
+            } else if (col.CompareTag("Atormentada")) {
+                if (saveChorona == null || saveChorona.name != col.name) {
+                    saveChorona = col.gameObject.GetComponent<cryingStatue>();
+                }
+                saveChorona.isVisible = true;
             }
         }
     }
 
-    string RayDetection(Vector3 direction, float maxDist) {
 
+    Collider RayDetection(Vector3 origin ,Vector3 direction, float maxDist) {
         RaycastHit hit;
         if (Physics.Raycast(transform.position, direction, out hit, maxDist)) {
-            if (debug) Debug.DrawRay(transform.position, direction * hit.distance, Color.green);
-            if (hit.collider.gameObject.tag == "Enemy") {
-                if (!saveEnemy)
-                    saveEnemy = hit.collider.gameObject.GetComponent<EnemyBehaviour>();
-                saveEnemy.Stop();
+            if (debug) Debug.DrawRay(origin, direction * hit.distance, Color.green);
+            return hit.collider;
+        }
+        return null;
+    }
+
+
+    public List<Collider> ConeShowerDetection(Transform origin, Vector3 direction, float range, float angle, int nCircles, int nRays) {
+        List<Collider> cols = new List<Collider>();
+        for (int c = 1; c < nCircles; c++) {
+
+            Vector3 dir2 = Quaternion.AngleAxis((( angle ) / (2 * nCircles)) * c, origin.up) * direction;
+            int nRaysAux = 1 + ((nRays - 1) / (nCircles - 1)) * c;
+            for (int i = 0; i < nRaysAux; i++) {
+                //rotacionar
+                Vector3 dir3 = Quaternion.AngleAxis(i * (360f / nRaysAux), origin.forward) * dir2;
+                Collider col = RayDetection(origin.position, dir3, range);
+                if (col != null && !cols.Contains(col)) {
+                    cols.Add(col);
+                }
             }
         }
-        return "FODEU DETECTOU NADA";
+        return cols;
     }
 
 
@@ -138,8 +156,8 @@ public class Lanterna : MonoBehaviour {
     }
 
     private void Recharge() {
-        if (ActualBattery < MaxBaterry) {
-            ActualBattery += BatteryRechargePerSecond * Time.deltaTime;
+        if (BaterryCounter < BaterryDuration) {
+            BaterryCounter += Time.deltaTime*(BaterryDuration/BaterryRecoveryDuration);
         }
     }
 
