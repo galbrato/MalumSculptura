66516449b72fda1230f93d073fdb560a88eb61c3
Rafael Monteiro <rafaelmonteiro95@gmail.com>
2018-11-09 07:29:15 -0200

    changed credits menu, and removed cursor lock on title screen

---
+++ Assets/MalumAssets/Scripts/AutoScroll.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AutoScroll : MonoBehaviour
+{
+
+    public float speed;
+    float scroll;
+    Scrollbar bar;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scroll = -speed;
+        bar = GetComponent<Scrollbar>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(bar.value <= 0.0f) scroll = speed;
+        if(bar.value >= 1.0f) scroll = -speed;
+    }
+
+    private void FixedUpdate() {
+        bar.value += scroll;
+    }
+}
